c424be47029b411cefecb5057ec6b01027351e6d
Andy <andrew.sunglae.kim@gmail.com>
2016-09-16 11:00:18 -0400

    adds tests/format

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,36 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+def ranks
+  ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+end
 
-players = []
+def suits
+  [ "hearts", "spades", "clubs", "diamonds" ]
+end
+
+def deck
+  ranks.product suits
+end
+
+def shuffle_deck
+  deck.shuffle
+end
+
+def get_players
+  players = []
+  while (player = gets.chomp) != "play"
+    players << {name: player, card: nil}
+  end
+  players
+end
+
+def play players
+  shuffled_deck = shuffle_deck
+  players.each do |player|
+    player[:card] = shuffled_deck.pop
+  end
+  players
+end
+
+def get_winner players
+  max_rank = players.max_by{|player| ranks.index(player[:card][0])}[:card][0]
+  winners = players.select{|player| player[:card][0] == max_rank}
+end
---
+++ spec/high_card_spec.rb
@@ -0,0 +1,49 @@
+def gets
+  ["bob","mary", "tom", "play"].sample
+end
+require_relative ('../high_card')
+require 'pry'
+
+
+describe "highcard" do
+  describe "deck" do
+    it "is an array" do
+      expect(deck.class).to eq(Array)
+    end
+    it "has 52 cards" do
+      expect(deck.length).to eq(52)
+    end
+    it "cards are in the following format: [rank, suit]" do
+      expect(deck.include?([2, "diamonds"])).to eq(true)
+    end
+    it "can be shuffled" do
+      expect(deck).not_to eq(shuffle_deck)
+    end
+  end
+
+  describe "players" do
+    players = get_players
+    it "is an array" do
+      expect(players.class).to eq(Array)
+    end
+    it "players are a hash" do
+      all_hash_players = players.select{|player| player.class == Hash}
+      expect(players.length == all_hash_players.length)
+    end
+  end
+
+  # breaks if play is sample first for players
+  describe "game play" do
+    players = play get_players
+    p players
+    it "should deal each player a card" do
+      sample_card_rank = players.sample[:card][0]
+      sample_card_suit = players.sample[:card][1]
+      expect(ranks.include?(sample_card_rank)).to eq(true)
+      expect(suits.include?(sample_card_suit)).to eq(true)
+    end
+    it "should determine winner" do
+      
+    end
+  end
+end
