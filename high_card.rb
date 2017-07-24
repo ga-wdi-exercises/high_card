ac025c4f3682757d86a84518b0b55f1077e6c125
John Master <john.s.master@gmail.com>
2017-07-24 17:16:37 -0400

    Update starter code
    Move the ace to the end of the starter array

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,5 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
